9d411119ad542163733b5955c8f3b5e46a901ab7
Jacob Shearer <MutantBadger@gmail.com>
2018-05-01 15:10:59 -0500

    Draw Modifications
    All draw methods now account for the flag showPointVisibility(0 and shows the definitive points for each drawable object, with Item being the obvious exception

--- src/NewSwingUI.java
+++ src/NewSwingUI.java
@@ -22,6 +22,16 @@ public class NewSwingUI implements UIContext {
     if (label.getStartingPoint() != null) {
       if (label.getText() != null) {
         graphics.drawString(label.getText(), (int) label.getStartingPoint().getX(), (int) label.getStartingPoint().getY());
+        
+        if (label.getPointVisibility()) {
+        	Color lastColor = graphics.getColor();
+	    	graphics.setColor(Color.BLACK);
+        	int x, y;
+        	x = Math.round((float)label.getStartingPoint().getX());
+        	y = Math.round((float)label.getStartingPoint().getY());
+        	graphics.drawRect(x-5, y-5, 10, 10);
+        	graphics.setColor(lastColor);
+        }
       }
     }
     int length = graphics.getFontMetrics().stringWidth(label.getText());
@@ -32,19 +42,23 @@ public class NewSwingUI implements UIContext {
     int i2 = 0;
     int i3 = 0;
     int i4 = 0;
-    if (line.getPoint1() != null) {
-      i1 = Math.round((float) (line.getPoint1().getX()));
-      i2 = Math.round((float) (line.getPoint1().getY()));
-      if (line.getPoint2() != null) {
-        i3 = Math.round((float) (line.getPoint2().getX()));
-        i4 = Math.round((float) (line.getPoint2().getY()));
-      } else {
-        i3 = i1;
-        i4 = i2;
-      }
-      graphics.drawLine(i1, i2, i3, i4);
-    }
+    if (line.getPoint1() != null && line.getPoint2() != null) {
+    	i1 = Math.round((float) (line.getPoint1().getX()));
+      	i2 = Math.round((float) (line.getPoint1().getY()));
+	    i3 = Math.round((float) (line.getPoint2().getX()));
+	    i4 = Math.round((float) (line.getPoint2().getY()));
+	    
+	    if (line.getPointVisibility()) {
+	    	Color lastColor = graphics.getColor();
+	    	graphics.setColor(Color.BLACK);
+	    	graphics.drawRect(i1-5,i2-5,10,10);
+	    	graphics.drawRect(i3-5,i4-5,10,10);
+	    	graphics.setColor(lastColor);
+	    }
+	    
+	    graphics.drawLine(i1, i2, i3, i4);
   }
+}
   public void draw(Ellipse ellipse) {
 	  int x = 0;
 	  int y = 0;
@@ -56,24 +70,34 @@ public class NewSwingUI implements UIContext {
 	  float p2x = 0;
 	  float p2y = 0;
 	  
-	  if(ellipse.getPoint1() != null) {
+	  if(ellipse.getPoint1() != null && ellipse.getPoint2() != null) {
 		  p1x = (float) ellipse.getPoint1().getX();
 		  p1y = (float) ellipse.getPoint1().getY();
-		  if(ellipse.getPoint2() != null) {
-			  p2x = (float) ellipse.getPoint2().getX();
-			  p2y = (float) ellipse.getPoint2().getY();
+		  p2x = (float) ellipse.getPoint2().getX();
+		  p2y = (float) ellipse.getPoint2().getY();
+		  
+		  if (ellipse.getPointVisibility()) {
+			  Color lastColor = graphics.getColor();
+			  graphics.setColor(Color.BLACK);
+			  int x1, y1, x2, y2;
+			  x1 = Math.round(p1x);
+			  y1 = Math.round(p1y);
+			  x2 = Math.round(p2x);
+			  y2 = Math.round(p2y);
 			  
-			  x = Math.round( Math.min(p1x, p2x) );
-			  y = Math.round( Math.min(p1y, p2y) );
-			  w = Math.abs( Math.round( Math.max(p1x, p2x) - Math.min(p1x, p2x) ) );
-			  h = Math.abs( Math.round( Math.max(p1y, p2y) - Math.min(p1y, p2y) ) );
-		  } else {
-			  p2x = p1x;
-			  p2y = p1y;
+			  graphics.drawRect(x1-5, y1-5, 10, 10);
+			  graphics.drawRect(x2-5, y2-5, 10, 10);
+			  graphics.setColor(lastColor);
 		  }
+		  
+		  x = Math.round( Math.min(p1x, p2x) );
+		  y = Math.round( Math.min(p1y, p2y) );
+		  w = Math.abs( Math.round( Math.max(p1x, p2x) - Math.min(p1x, p2x) ) );
+		  h = Math.abs( Math.round( Math.max(p1y, p2y) - Math.min(p1y, p2y) ) );
 		  graphics.drawOval(x, y, w, h);
 	  }
-  }
+}
+  
   
   public void draw(BSpline spline) {
 	  
